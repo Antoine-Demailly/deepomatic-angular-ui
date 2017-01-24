8aa0662b6f079d12eca3e0c801e102bee2d5e546
Antoine Demailly <antoine.demailly@gmx.fr>
2017-01-24 18:23:43 +0100

    Basic url upload

--- js/components/layout/layout.controller.js
+++ js/components/layout/layout.controller.js
@@ -16,6 +16,7 @@
     vm.detectButtonDisable = true;
 
     vm.picture = '';
+    vm.urlInputValue = '';
     vm.boxes = [];
 
     /// Public Methods
@@ -23,6 +24,7 @@
 
     vm.detect = detect;
     vm.upload = upload;
+    vm.urlInput = urlInput;
 
     function detect() {
       console.log('Detect');
@@ -32,6 +34,29 @@
       Upload.base64DataUrl(file).then(function(fileBase64) {
         vm.picture = fileBase64;
       });
+    }
+
+    function urlInput() {
+      console.log('hello');
+      validData();
+    }
+
+    function validateUrl(value) {
+      return /^(https?|ftp):\/\/(((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:)*@)?(((\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5])\.(\d|[1-9]\d|1\d\d|2[0-4]\d|25[0-5]))|((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?)(:\d*)?)(\/((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)+(\/(([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)*)*)?)?(\?((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|[\uE000-\uF8FF]|\/|\?)*)?(\#((([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(%[\da-f]{2})|[!\$&'\(\)\*\+,;=]|:|@)|\/|\?)*)?$/i.test(value);
+    }
+
+    /// Private Methods
+    ///////
+
+    function validData() {
+      if (vm.picture.length) {
+        vm.detectButtonDisable = false;
+      } else if (vm.urlInputValue.length && validateUrl(vm.urlInputValue)) {
+        vm.detectButtonDisable = false;
+        vm.picture = vm.urlInputValue;
+      } else {
+        vm.detectButtonDisable = true;
+      }
     }
   }
 })();
