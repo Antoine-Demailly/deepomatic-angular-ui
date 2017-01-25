a1adfc9c8f87b9f074ba422e0f8d76af58e8ae04
Antoine Demailly <antoine.demailly@gmx.fr>
2017-01-25 01:52:33 +0100

    Box positions

--- js/components/detector/box.directive.js
+++ js/components/detector/box.directive.js
@@ -10,16 +10,22 @@
     var directive = {
       restrict: 'A',
       link: linkFunc,
+      scope: {
+        positions: '=',
+      }
     };
 
     return directive;
 
     function linkFunc(scope, el) {
+      console.log('positions', scope.positions);
+
       $rootScope.$broadcast('newBox');
 
       $rootScope.$on('receiveOffset', function(e, offset) {
         console.log('offset', offset);
       });
+
     }
   }
 })();
